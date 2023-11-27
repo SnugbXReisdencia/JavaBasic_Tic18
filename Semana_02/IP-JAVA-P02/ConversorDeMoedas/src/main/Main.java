7f5dc544f826fb2dc4c3e21491a79e8dcaf33687
SnugbXReisdencia <santoseverlan45@gmail.com>
2023-11-27 19:37:06 -0300

    Atualização de apps

---
+++ Semana_02/IP-JAVA-P02/ConversorDeMoedas/src/main/Main.java
@@ -0,0 +1,16 @@
+package main;
+
+public class Main {
+
+	public static void main(String[] args) {
+		//#Conversor de Moedas
+
+	}
+	
+	public static void Dola_e_Real(double valor) {
+		//Conversor de Dolar para Real
+		
+		
+	}
+
+}
