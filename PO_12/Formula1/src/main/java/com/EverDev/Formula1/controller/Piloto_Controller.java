cc0d1cf7fb6bc98352c37647953da97adcb77d4c
SnugbXReisdencia <santoseverlan45@gmail.com>
2024-02-15 20:42:06 -0300

    add endpoints /porpais e /mediaporpais e os metodos getPorPais getMediaPorpais, getVencedoresPorPais e sortByValue

--- PO_12/Formula1/src/main/java/com/EverDev/Formula1/controller/Piloto_Controller.java
+++ PO_12/Formula1/src/main/java/com/EverDev/Formula1/controller/Piloto_Controller.java
@@ -1,7 +1,12 @@
 package com.EverDev.Formula1.controller;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,7 +27,6 @@ public class Piloto_Controller {
 	
 	@GetMapping("/todos")
 	public String vencedor() throws IOException {
-		vencedor += "<H1>Pilotos Formula 1</H1><br>";
 		vencedor += "<h2>Lista de todos os pilotos:</h2>";
 		vencedor += "<table class=\"table table-striped\">";
 		vencedor += "<tr><th>Nome</th><th>Pais</th><th>Vitorias</th></tr>";
@@ -78,4 +82,87 @@ public class Piloto_Controller {
 		return retorno;
 	}
 	
+	@GetMapping("/porPais")
+	public String porPais() throws IOException {
+		retorno = vencedor;
+		retorno += "<h2>Pilotos por Pais:</h2>";
+		retorno += "<table class=\"table table-striped table-hover\">";
+		retorno += "<tr><th>Pais</th><th>Vitorias</th></tr>";
+		Map<String, Integer> vitoriasPorPais = getPorPais();
+		for (Map.Entry<String, Integer> entry : vitoriasPorPais.entrySet()) {
+			retorno += "<tr><td>" + entry.getKey() + "</td><td>" + entry.getValue() + "</td></tr>";
+		}
+		retorno += "</table>";
+	    return retorno;
+	}
+	
+	@GetMapping("/mediaporpais")
+	public String mediaPorPais() throws IOException {
+		retorno = vencedor;
+		retorno += "<h2>Media de vitorias por Pais:</h2>";
+		retorno += "<table class=\"table table-striped table-hover\">";
+		retorno += "<tr><th>Pais</th><th>Media</th></tr>";
+		Map<String, Double> mediaPorPais = getMediaPorPais();
+		for (Map.Entry<String, Double> entry : mediaPorPais.entrySet()) {
+			retorno += "<tr><td>" + entry.getKey() + "</td><td>" + String.format("%.2f", entry.getValue()).replace(",", ".") + "</td></tr>";
+		}
+		retorno += "</table>";
+	    return retorno;
+	}
+	
+	private Map<String, Integer> getPorPais() throws IOException {
+	    Map<String, Integer> vitoriasPorPais = new HashMap<>();
+	    for (Piloto piloto : pilotos) {
+	        String pais = piloto.getPais();
+	        int vitorias = piloto.getVitorias();
+
+	        vitoriasPorPais.put(pais, vitoriasPorPais.getOrDefault(pais, 0) + vitorias);
+	    }
+
+	    // Ordena o mapa por número de vitórias (decrescente)
+	    vitoriasPorPais = sortByValue(vitoriasPorPais);
+
+	    return vitoriasPorPais;
+	}
+
+	private Map<String, Double> getMediaPorPais() throws IOException {
+	    Map<String, Integer> vitoriasPorPais = getPorPais();
+	    Map<String, Integer> vencedoresPorPais = getVencedoresPorPais();
+
+	    Map<String, Double> mediaPorPais = new HashMap<>();
+	    for (Map.Entry<String, Integer> entry : vitoriasPorPais.entrySet()) {
+	        String pais = entry.getKey();
+	        int vitorias = entry.getValue();
+	        int vencedores = vencedoresPorPais.get(pais);
+
+	        mediaPorPais.put(pais, (double) vitorias / vencedores);
+	    }
+
+	    // Ordena o mapa por média de vitórias (decrescente)
+	    mediaPorPais = sortByValue(mediaPorPais);
+
+	    return mediaPorPais;
+	}
+	
+	private Map<String, Integer> getVencedoresPorPais() throws IOException {
+	    Map<String, Integer> vencedoresPorPais = new HashMap<>();
+	    for (Piloto piloto : pilotos) {
+	        String pais = piloto.getPais();
+	        vencedoresPorPais.put(pais, vencedoresPorPais.getOrDefault(pais, 0) + 1);
+	    }
+
+	    return vencedoresPorPais;
+	}
+	
+	private static <K, V extends Comparable<? super V>> Map<K, V> sortByValue(Map<K, V> map) {
+	    List<Map.Entry<K, V>> list = new ArrayList<>(map.entrySet());
+	    list.sort(Map.Entry.comparingByValue(Comparator.reverseOrder()));
+
+	    Map<K, V> result = new LinkedHashMap<>();
+	    for (Map.Entry<K, V> entry : list) {
+	        result.put(entry.getKey(), entry.getValue());
+	    }
+
+	    return result;
+	}
 }
