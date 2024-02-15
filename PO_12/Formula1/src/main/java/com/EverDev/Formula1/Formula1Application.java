1fd053abb11364dcf4bad0c98751f76bce4728bd
SnugbXReisdencia <santoseverlan45@gmail.com>
2024-02-14 22:36:12 -0300

    add pratica 12

---
+++ PO_12/Formula1/src/main/java/com/EverDev/Formula1/Formula1Application.java
@@ -0,0 +1,13 @@
+package com.EverDev.Formula1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Formula1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Formula1Application.class, args);
+	}
+
+}
---
+++ PO_12/Formula1/src/test/java/com/EverDev/Formula1/Formula1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.EverDev.Formula1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Formula1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
