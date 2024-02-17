920fe876a5d92a5d74754ed4a7bb7362a0df2af9
SnugbXReisdencia <santoseverlan45@gmail.com>
2024-02-17 19:37:20 -0300

    add PO_13

---
+++ PO_13/CompaniaAerea/CompaniaAerea/src/main/java/com/EverDev/CompaniaAerea/CompaniaAereaApplication.java
@@ -0,0 +1,13 @@
+package com.EverDev.CompaniaAerea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompaniaAereaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompaniaAereaApplication.class, args);
+	}
+
+}
---
+++ PO_13/CompaniaAerea/CompaniaAerea/src/test/java/com/EverDev/CompaniaAerea/CompaniaAereaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.EverDev.CompaniaAerea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompaniaAereaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
