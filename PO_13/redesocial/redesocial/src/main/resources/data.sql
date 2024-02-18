16cb71dfdbb09632f4da1f77da75983f43093832
SnugbXReisdencia <santoseverlan45@gmail.com>
2024-02-18 14:26:13 -0300

    add data base

---
+++ PO_13/redesocial/redesocial/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO USUARIO (name, email, senha) VALUES('mestre', 'mestre@email.com', 'ever');
+INSERT INTO USUARIO (name, email, senha) VALUES('zangado', 'zangado@email.com', 'ever');
+INSERT INTO USUARIO (name, email, senha) VALUES('atchim', 'atchim@email.com', 'ever');
+INSERT INTO USUARIO (name, email, senha) VALUES('soneca', 'soneca@email.com', 'ever');
