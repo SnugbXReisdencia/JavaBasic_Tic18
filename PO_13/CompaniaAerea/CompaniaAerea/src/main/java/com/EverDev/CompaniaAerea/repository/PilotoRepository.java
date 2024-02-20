02d5ae1683399c3983965c2a303d6a8622812d88
SnugbXReisdencia <santoseverlan45@gmail.com>
2024-02-20 11:33:43 -0300

    atualizando interface PilotoRepository

--- PO_13/CompaniaAerea/CompaniaAerea/src/main/java/com/EverDev/CompaniaAerea/repository/PilotoRepository.java
+++ PO_13/CompaniaAerea/CompaniaAerea/src/main/java/com/EverDev/CompaniaAerea/repository/PilotoRepository.java
@@ -1,9 +1,13 @@
 package com.EverDev.CompaniaAerea.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.EverDev.CompaniaAerea.model.Piloto;
 
 public interface PilotoRepository extends JpaRepository<Piloto, Integer> {
 
+	List<Piloto> findByNome(String nome);
+	
 }
