08c535ad0b6608244a06be44abc2fe974ff1d266
SnugbXReisdencia <santoseverlan45@gmail.com>
2024-02-20 11:40:35 -0300

    add classe AeroportoController

---
+++ PO_13/CompaniaAerea/CompaniaAerea/src/main/java/com/EverDev/CompaniaAerea/controller/AeroportoController.java
@@ -0,0 +1,33 @@
+package com.EverDev.CompaniaAerea.controller;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.EverDev.CompaniaAerea.controller.dto.AeroportoDTO;
+import com.EverDev.CompaniaAerea.model.Aeroporto;
+import com.EverDev.CompaniaAerea.repository.AeroportoRepository;
+
+@RestController
+public class AeroportoController {
+	
+	@Autowired
+	private AeroportoRepository aeroportoRepository;
+	
+	@RequestMapping("/aeroportos/")
+	public List<AeroportoDTO> getAeroportos(String icao) {
+		
+		List<Aeroporto> aeroportos;
+		if (icao != null) {
+			aeroportos = aeroportoRepository.findByICAO(icao); 
+		}else {
+			aeroportos = aeroportoRepository.findAll();
+		}
+		
+		return aeroportos.stream().map(AeroportoDTO::new).collect(Collectors.toList());
+		
+	}
+}
