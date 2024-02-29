66433b25de23bad4dfd652e5ad2030e827beb23d
SnugbXReisdencia <santoseverlan45@gmail.com>
2024-02-29 20:40:52 -0300

    add avalicão

--- AvaliacaoJavaTrilha/Leilao/Leilao/src/main/java/com/EverDev/Leilao/repository/LanceRespository.java
+++ AvaliacaoJavaTrilha/Leilao/Leilao/src/main/java/com/EverDev/Leilao/repository/LanceRespository.java
@@ -1,12 +1,10 @@
 package com.EverDev.Leilao.repository;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.EverDev.Leilao.model.Lance;
 
 public interface LanceRespository extends JpaRepository<Lance, Long> {
 
-	List<Lance> findByLeilaoId(Long idLeilao);
+	
 }
